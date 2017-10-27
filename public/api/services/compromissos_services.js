20b87e4d35519f1f6ed0801a45b78df98cdb843a
Thiago Martins <Thiago Martins>
2017-10-27 03:21:11 -0200

    Refactoring de código modfiicações de tela

--- public/api/services/compromissos_services.js
+++ public/api/services/compromissos_services.js
@@ -1,10 +1,10 @@
 'use strict';
 
 var mongoose = require('mongoose');
-var model = mongoose.model('Compromissos');
+var Compromisso = mongoose.model('Compromissos');
 
 exports.listar_todos = function(req, res) {    
-   model.find(function(err, compromissos) {
+   Compromisso.find(function(err, compromissos) {
       if (err){
           res.send(err)
       }
@@ -12,11 +12,12 @@ exports.listar_todos = function(req, res) {
   });
 };
 
-exports.criar_compromisso = function(req, res) {  
-  model.create({  
-    titulo : req.body.titulo,
-    descricao : req.body.descricao,
-    data : req.body.data
+exports.criar_compromisso = function(req, res) {
+  console.log(req.body);
+  Compromisso.create({
+            titulo : req.body.titulo,
+            descricao : req.body.descricao,
+            data : req.body.data
         }, function(err, compromisso) {            
     if (err){
         res.send(err)
@@ -28,12 +29,13 @@ exports.criar_compromisso = function(req, res) {
 
 exports.buscar_compromisso = function(req, res) {
   var id = mongoose.Types.ObjectId(req.query.id); 
-  model.findById({_id:id}, function(err, compromissos) {
-    if (err){
-        res.send(err)
-    }
-    res.json(compromissos); 
-  });
+  console.log(id);  
+    Compromisso.findById({_id:id}, function(err, compromissos) {
+        if (err)
+            res.send(err)
+
+        res.json(compromissos); 
+    });
   /*Compromisso.findOne({_id:id}, function(err, compromisso) {
     if (err)
       return err;
@@ -45,12 +47,9 @@ exports.buscar_compromisso = function(req, res) {
 
 
 exports.atualizar_compromisso = function(req, res) {
-  model.findOneAndUpdate({
-    _id: req.query.id
-    }, req.body, { new: true }, function(err, compromisso) {
-    if (err){
+  Compromisso.findOneAndUpdate({_id: req.query.id}, req.body, {new: true}, function(err, compromisso) {
+    if (err)
       return err;
-    } 
     return compromisso;
   });
 };
@@ -58,7 +57,7 @@ exports.atualizar_compromisso = function(req, res) {
 
 exports.apagar_compromisso = function(req, res) {
   var id = mongoose.Types.ObjectId(req.params.id); 
-  model.remove({
+  Compromisso.remove({
     _id: id
   }, function(err, compromissos) {
       if (err){
