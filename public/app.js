c8dc0b2c901aa31929b7371836931e1520e1a40b
Thiago Martins <Thiago Martins>
2017-10-29 18:35:14 -0200

    versionando o arquivo app.js que não havia subido

---
+++ public/app.js
@@ -0,0 +1,59 @@
+var moduloCompromissos = angular.module('moduloCompromissos', []);
+
+function mainController($scope, $http) {
+    let vm = $scope;
+     
+    vm.compromisso = {};
+    vm.compromissos = [];
+    vm.obterCompromissos = obtemCompromissos;    
+    vm.adicionarCompromisso = incluirCompromisso;
+    vm.excluirCompromisso = excluiCompromisso;
+    vm.editarCompromisso = editaCompromisso;
+
+    vm.obterCompromissos();
+
+    function obtemCompromissos(){                
+        $http.get('/compromissos/').then(function(dados){            
+            vm.compromissos = dados.data;
+        });        
+    }
+
+    function incluirCompromisso(){               
+        var compromissoAdicao = {
+          titulo:vm.compromisso.titulo,
+          descricao:vm.compromisso.descricao,
+          data:vm.compromisso.data
+        };
+        $http.post('/compromissos/', compromissoAdicao).then(function(data){
+             vm.obterCompromissos();
+        });  
+    }
+    
+    function excluiCompromisso(id){        
+        $http.delete('/compromissos/' + id).then(function(data){
+             vm.obterCompromissos();
+        });  
+    }
+
+    function validarPreenchimentoCamposObrigatorios(){
+        if(validaPreenchimentoCampo(vm.titulo)){
+            return "Necessário preenchimento do título!";
+        } else if (validaPreenchimentoCampo(vm.descricao)){
+            return "Necessário preenchimento do título!";
+        } else if (validaPreenchimentoCampo(vm.data)){
+            return "Necessário preenchimento da Data e Hora!";
+        }
+    }
+
+    function validaPreenchimentoCampo(campo){
+         if(campo == undefined || campo.length ==0) return false;
+    }
+
+    function editaCompromisso(id){                
+        $http.post('/compromissos/'+ id).then(function(dados){            
+            vm.compromisso = dados.data;
+        });          
+    }
+
+}
+    
