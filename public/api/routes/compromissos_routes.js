bb4c22525f8e97a937f100ea21ed4f9b0874cf17
Thiago Martins <Thiago Martins>
2017-10-29 18:34:32 -0200

    versionando o arquivo app.js que não havia subido

--- public/api/routes/compromissos_routes.js
+++ public/api/routes/compromissos_routes.js
@@ -2,10 +2,10 @@
 module.exports = function(app) {
   var services = require('../services/compromissos_services');
 
-	app.get('/compromissos', services.listar_todos);
-    app.post('/compromissos', services.criar_compromisso);
-    app.post('/compromissos/:id', services.buscar_compromisso);
-    app.put('/compromissos', services.atualizar_compromisso);
-    app.delete('/compromissos/:id', services.apagar_compromisso);
+  app.get('/compromissos', services.listar_todos);
+  app.post('/compromissos', services.criar_compromisso);
+  app.post('/compromissos/:id', services.buscar_compromisso);
+  app.put('/compromissos', services.atualizar_compromisso);
+  app.delete('/compromissos/:id', services.apagar_compromisso);
   
 };
